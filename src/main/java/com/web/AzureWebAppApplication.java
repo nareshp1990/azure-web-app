8663119ce31f045c1982159827769de32d3b980b
Naresh, Patchipulusu <naresh_patchipulusu@optum.com>
2021-07-20 22:03:32 +0530

    Welcome page added

--- src/main/java/com/web/AzureWebAppApplication.java
+++ src/main/java/com/web/AzureWebAppApplication.java
@@ -2,20 +2,12 @@ package com.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class AzureWebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AzureWebAppApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String sayHello(){
-		return "Hello World!..";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AzureWebAppApplication.class, args);
+    }
 
 }
